53fbdf2ae1407803fdcf697a5f7532a276aea6c1
Hugo Leclerc <hugal.leclerc@gmail.com>
2022-10-13 21:39:25 +0200

    direct python

--- bld.bat
+++ bld.bat
@@ -2,5 +2,5 @@ mkdir -p ext
 
 git clone https://github.com/eigenteam/eigen-git-mirror.git ext/eigen3
 git clone https://github.com/sd-ot/sdot.git ext/sdot
-"%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
+python setup.py install --single-version-externally-managed --record=record.txt
 
