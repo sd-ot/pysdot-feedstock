c9c49c86615a6c79b4bee6db0eb6fd99d94935cb
Hugo Leclerc <hugal.leclerc@gmail.com>
2022-10-13 17:51:09 +0200

    setup.py

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# if [[ $(uname) == Darwin ]]; then
-#     EXTRA_ARGS="--without-threads"
-# fi
-
 mkdir -p ext
 git clone https://github.com/sd-ot/sdot.git ext/sdot
 git clone https://github.com/eigenteam/eigen-git-mirror.git ext/eigen3
-python setup.py build
-python setup.py install
+# python setup.py build
+python setup.py install --single-version-externally-managed --record=record.txt
