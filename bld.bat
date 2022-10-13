fefc3b33cd1d4dacb48e25ab32349eae29875464
Hugo Leclerc <hugal.leclerc@gmail.com>
2022-10-13 21:56:02 +0200

    loc pybind11

--- bld.bat
+++ bld.bat
@@ -1,6 +1,7 @@
 mkdir -p ext
 
 git clone https://github.com/eigenteam/eigen-git-mirror.git ext/eigen3
+git clone https://github.com/pybind/pybind11.git ext/pybind11
 git clone https://github.com/sd-ot/sdot.git ext/sdot
 python setup.py install --single-version-externally-managed --record=record.txt
 
